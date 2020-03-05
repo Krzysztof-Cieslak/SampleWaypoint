6ffbbca7e0496e9c9bb3711b16b80fe05b65807f
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-03-05 23:11:34 +0100

    Bump version to 0.0.3

--- src/SampleWaypoint/AssemblyInfo.fs
+++ src/SampleWaypoint/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SampleWaypoint")>]
 [<assembly: AssemblyProductAttribute("SampleWaypoint")>]
 [<assembly: AssemblyDescriptionAttribute("Sample application create by Waypoint")>]
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "SampleWaypoint"
     let [<Literal>] AssemblyProduct = "SampleWaypoint"
     let [<Literal>] AssemblyDescription = "Sample application create by Waypoint"
-    let [<Literal>] AssemblyVersion = "0.0.2"
-    let [<Literal>] AssemblyFileVersion = "0.0.2"
+    let [<Literal>] AssemblyVersion = "0.0.3"
+    let [<Literal>] AssemblyFileVersion = "0.0.3"
