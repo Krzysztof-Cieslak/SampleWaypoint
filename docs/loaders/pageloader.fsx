f309239f4a70393c0801a503c9db8a690bf13f16
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-05-18 17:36:25 +0200

    Fix

--- docs/loaders/pageloader.fsx
+++ docs/loaders/pageloader.fsx
@@ -6,7 +6,7 @@ type Shortcut = {
     link: string
     icon: string
 }
-`
+
 let loader (projectRoot: string) (siteContet: SiteContents) =
     siteContet.Add({title = "Home"; link = "http://kcieslak.io/SampleWaypoint"; icon = "fas fa-home"})
     siteContet.Add({title = "GitHub repo"; link = "https://github.com/Krzysztof-Cieslak/SampleWaypoint"; icon = "fab fa-github"})
